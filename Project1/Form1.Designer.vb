7592ca204db5ae345a6996574b3e77a430a868dd
nazmirul <nazmirul_ichigo@yahoo.com>
2024-10-15 02:03:31 +0800

    commit

---
+++ Project1/Form1.Designer.vb
@@ -0,0 +1,254 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.LabelHMIStatus = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.LabelViewStatus = New System.Windows.Forms.Label()
+        Me.LabelWwalmloggerStatus = New System.Windows.Forms.Label()
+        Me.LabelAlmprtStatus = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.ButtonAbout = New System.Windows.Forms.Button()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(42, 273)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(81, 31)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Launch HMI"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(129, 234)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(57, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(320, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Wonderware InTouch Project Launcher"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(39, 234)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(84, 16)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Select Plant :"
+        '
+        'LabelHMIStatus
+        '
+        Me.LabelHMIStatus.AutoSize = True
+        Me.LabelHMIStatus.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelHMIStatus.Location = New System.Drawing.Point(133, 56)
+        Me.LabelHMIStatus.Name = "LabelHMIStatus"
+        Me.LabelHMIStatus.Size = New System.Drawing.Size(39, 13)
+        Me.LabelHMIStatus.TabIndex = 4
+        Me.LabelHMIStatus.Text = "Label3"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(277, 273)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(81, 31)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Force Close"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'LabelViewStatus
+        '
+        Me.LabelViewStatus.AutoSize = True
+        Me.LabelViewStatus.Location = New System.Drawing.Point(138, 172)
+        Me.LabelViewStatus.Name = "LabelViewStatus"
+        Me.LabelViewStatus.Size = New System.Drawing.Size(39, 13)
+        Me.LabelViewStatus.TabIndex = 6
+        Me.LabelViewStatus.Text = "Label3"
+        '
+        'LabelWwalmloggerStatus
+        '
+        Me.LabelWwalmloggerStatus.AutoSize = True
+        Me.LabelWwalmloggerStatus.Location = New System.Drawing.Point(126, 110)
+        Me.LabelWwalmloggerStatus.Name = "LabelWwalmloggerStatus"
+        Me.LabelWwalmloggerStatus.Size = New System.Drawing.Size(39, 13)
+        Me.LabelWwalmloggerStatus.TabIndex = 7
+        Me.LabelWwalmloggerStatus.Text = "Label3"
+        '
+        'LabelAlmprtStatus
+        '
+        Me.LabelAlmprtStatus.AutoSize = True
+        Me.LabelAlmprtStatus.Location = New System.Drawing.Point(186, 142)
+        Me.LabelAlmprtStatus.Name = "LabelAlmprtStatus"
+        Me.LabelAlmprtStatus.Size = New System.Drawing.Size(39, 13)
+        Me.LabelAlmprtStatus.TabIndex = 8
+        Me.LabelAlmprtStatus.Text = "Label3"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(43, 54)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(77, 16)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "HMI Status :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(43, 84)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(119, 16)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Process Status :"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(58, 108)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(62, 15)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "View.exe :"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(58, 140)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(122, 15)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "WWALMLogger.exe :"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(58, 170)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(74, 15)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "Almprt.exe : "
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.ButtonAbout)
+        Me.Panel1.Location = New System.Drawing.Point(12, 215)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(393, 150)
+        Me.Panel1.TabIndex = 14
+        '
+        'ButtonAbout
+        '
+        Me.ButtonAbout.Location = New System.Drawing.Point(344, 125)
+        Me.ButtonAbout.Name = "ButtonAbout"
+        Me.ButtonAbout.Size = New System.Drawing.Size(47, 23)
+        Me.ButtonAbout.TabIndex = 0
+        Me.ButtonAbout.Text = "About"
+        Me.ButtonAbout.UseVisualStyleBackColor = True
+        '
+        'Panel2
+        '
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel2.Location = New System.Drawing.Point(13, 37)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(392, 163)
+        Me.Panel2.TabIndex = 15
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(423, 380)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.LabelAlmprtStatus)
+        Me.Controls.Add(Me.LabelWwalmloggerStatus)
+        Me.Controls.Add(Me.LabelViewStatus)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.LabelHMIStatus)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Panel2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.HelpButton = True
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Wonderware InTouch Project Launcher"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Windows.Forms.Button
+    Friend WithEvents ComboBox1 As Windows.Forms.ComboBox
+    Friend WithEvents Label1 As Windows.Forms.Label
+    Friend WithEvents Label2 As Windows.Forms.Label
+    Friend WithEvents LabelHMIStatus As Windows.Forms.Label
+    Friend WithEvents Button2 As Windows.Forms.Button
+    Friend WithEvents LabelViewStatus As Windows.Forms.Label
+    Friend WithEvents LabelWwalmloggerStatus As Windows.Forms.Label
+    Friend WithEvents LabelAlmprtStatus As Windows.Forms.Label
+    Friend WithEvents Label3 As Windows.Forms.Label
+    Friend WithEvents Label4 As Windows.Forms.Label
+    Friend WithEvents Label5 As Windows.Forms.Label
+    Friend WithEvents Label6 As Windows.Forms.Label
+    Friend WithEvents Label7 As Windows.Forms.Label
+    Friend WithEvents Panel1 As Windows.Forms.Panel
+    Friend WithEvents ButtonAbout As Windows.Forms.Button
+    Friend WithEvents Panel2 As Windows.Forms.Panel
+End Class
